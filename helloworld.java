3d9e4436224ef5a605a4ca571634e148c74ed5cd
Praneethallam <45547873+Praneethallam@users.noreply.github.com>
2021-06-14 10:22:47 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-	System.out.println("Changes happening in score webhook..!!!!!!!!!");
+	System.out.println("Changes happening in score webhook");
  //creating object	
   }
 }
